a21f1e0815c8f2bd97586d53d22e860054658e0c
sbroers <44463486+sbroers@users.noreply.github.com>
2018-10-26 10:10:14 +0200

    Delete LastLogOnDate.ps1

--- LastLogOnDate.ps1
+++
@@ -1,4 +0,0 @@
-$UserPath = "$($env:USERPROFILE)\Desktop\lastlogon.csv"
-
-Get-ADUser -Filter * -Properties LastLogonDate | Sort-Object -Property LastLogonDate -descending | Select-Object Name, LastLogonDate |
-Export-Csv -NoTypeInformation -Path $UserPath -UseCulture
